58fbe5103eb811718171eaa5325ac70ac19a43a2
Vladimir Daniuk <vovand1996@gmail.com>
2018-04-27 22:31:09 +0300

    add(middleware): check-auth

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 const Order = require('../models/order');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
   Order.find().select('product quantity _id').populate('product', 'name').exec()
     .then(docs => {
       res.status(200).json({
@@ -26,7 +27,7 @@ router.get('/', (req, res, next) => {
     .catch();
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
   Product.findById(req.body.productId)
     .then(product => {
       if (!product) {
@@ -60,7 +61,7 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
   Order.findById(req.params.orderId).populate('product').exec()
     .then(order => {
       if (!order) {
@@ -83,7 +84,7 @@ router.get('/:orderId', (req, res, next) => {
     });
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
   Order.remove({_id: req.params.orderId}).exec()
     .then(result => {
       res.status(200).json({
